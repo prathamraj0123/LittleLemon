b697141b44fc1e85dcabd0c2e715c3667ccf9050
Pratham Raj <prathamraj0123@gmail.com>
2023-03-22 15:51:06 +0530

    mysql database configured and model defined

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Booking(models.Model):
+    name = models.CharField(max_length=255)
+    no_of_guests = models.SmallIntegerField()
+    booking_date = models.DateTimeField(db_index=True)
+    
+    def __str__(self) -> str:
+        return self.name
+    
+class Menu(models.Model):
+    title = models.CharField(max_length=255, db_index=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2, db_index=True)
+    inventory = models.SmallIntegerField()
+    
+    def __str__(self) -> str:
+        return self.title
