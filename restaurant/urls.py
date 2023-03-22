ca7c1c2218cc622e2032ca056f3bdaafcaa40329
Pratham Raj <prathamraj0123@gmail.com>
2023-03-22 18:53:29 +0530

    created view for menu and booking model

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
+from rest_framework.routers import DefaultRouter
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('menu/', views.MenuItemView.as_view()),
+    path('menu/<int:pk>', views.SingleMenuItemView.as_view()),
+
 ]
