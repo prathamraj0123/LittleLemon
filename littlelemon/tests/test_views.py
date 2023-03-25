b4f39bcbcac978e3c1a75bdca8793094e516ed21
Pratham Raj <prathamraj0123@gmail.com>
2023-03-25 19:05:56 +0530

    last_commit

--- littlelemon/tests/test_views.py
+++ littlelemon/tests/test_views.py
@@ -9,17 +9,21 @@ class MenuItemViewTest(TestCase):
     def setUp(self) -> None:
         MenuItem.objects.create(title="fried fish", price="7.50", inventory="50")       
         MenuItem.objects.create(title="chicken roll", price="4.50", inventory="60")
-        # MenuItem.objects.create(title="butter chicken", price="8.50", inventory="50")        
-        # MenuItem.objects.create(title="chole bhature", price="7.50", inventory="5")
         
     def test_get_all(self):
         retrieved_object = MenuItem.objects.all()
 
         serialized_object = MenuItemSerializer(retrieved_object, many=True)
-        serialized_object = json.loads(json.dumps(serialized_object.data))
+
+        response = self.client.get(reverse('menu-items')) 
         
-        response = self.client.get(reverse('menu-items'))       
-        response = str(response.content, 'UTF-8')
-        response = json.loads(response)
+        self.assertEqual(response.data, serialized_object.data)
         
-        self.assertEqual(response, serialized_object)
+        # Another method to test view data.
+        
+        # serialized_object = json.loads(json.dumps(serialized_object.data))      
+        # response = str(response.content, 'UTF-8')
+        # response = json.loads(response)
+        
+        # self.assertEqual(response, serialized_object)
+        
